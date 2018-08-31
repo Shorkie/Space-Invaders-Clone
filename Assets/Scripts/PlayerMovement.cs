cc3b568afbe33d0f7caa3d250b1c57835da818aa
Shorkie <shorkiedokey@gmail.com>
2018-08-31 09:29:31 -0300

    Can't fetch/pull unless I commit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,28 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
-public GameObject bulletSpawnPosition;
-public GameObject bullet;
-	// Use this for initialization
-	void Start () {	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		//Moving left and preventing movement beyond the boundaries 
-		if (Input.GetKey(KeyCode.LeftArrow) && (this.transform.position.x  >= -6.7f)){
-		Vector3 pos = transform.position;
-		pos.x -= 0.1f;
-		transform.position = pos;	
-		//Moving right and preventing movement beyond the boundaries 	 
-		} else if (Input.GetKey(KeyCode.RightArrow) && (this.transform.position.x  <= 6.67f)){
-		Vector3 pos = transform.position;
-		pos.x += 0.1f;
-		transform.position = pos;
-		}
-		//Shootin'
-		if (Input.GetKeyDown(KeyCode.Space)){
-			Instantiate(bullet, bulletSpawnPosition.transform.position, bulletSpawnPosition.transform.rotation);
-		}
-	}
+    public GameObject bulletSpawnPosition;
+    public GameObject bullet;
+    public float playerSpeed;
+    // Use this for initialization
+    void Start () { }
+
+    // Update is called once per frame
+    void Update () {
+        //Moving left and preventing movement beyond the boundaries 
+        if (Input.GetKey (KeyCode.LeftArrow) && (this.transform.position.x >= -6.7f)) {
+            Vector3 pos = transform.position;
+            pos.x -= playerSpeed * Time.deltaTime;
+            transform.position = pos;
+            //Moving right and preventing movement beyond the boundaries 	 
+        } else if (Input.GetKey (KeyCode.RightArrow) && (this.transform.position.x <= 6.67f)) {
+            Vector3 pos = transform.position;
+            pos.x += playerSpeed * Time.deltaTime;
+            transform.position = pos;
+        }
+        //Shootin'
+        if (Input.GetKeyDown (KeyCode.Space)) {
+            Instantiate (bullet, bulletSpawnPosition.transform.position, bulletSpawnPosition.transform.rotation);
+        }
+    }
 }
